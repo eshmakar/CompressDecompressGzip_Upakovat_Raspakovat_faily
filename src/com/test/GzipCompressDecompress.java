33addaf959fabfc5261ed5d40eabd71292a2356e
hasanshaih <hafizov.marat@mail.ru>
2021-07-14 14:44:47 +0300

    fixed bags

--- src/com/test/GzipCompressDecompress.java
+++ src/com/test/GzipCompressDecompress.java
@@ -5,31 +5,47 @@ import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class GzipCompressDecompress {
-    private static final String INPUT_FILE_TO_COMPRESS = "C:/hello.txt";
-    private static final String INPUT_COMPRESS_FILE = "C:/prints.txt.gzip";
+    private static final String COMPRESSED_FILE = "C:/big.txt.gzip";
+    private static final String FILE = "C:/big.txt";
 
-    public static void decompressGzip() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(INPUT_COMPRESS_FILE))));
-        String s;
-        while ((s = in.readLine()) != null)
-            System.out.println(s);
-        in.close();
+
+    public static void unGzip() {
+        long start = System.currentTimeMillis();
+        byte[] buffer = new byte[2048];
+        try (GZIPInputStream gZIPInput = new GZIPInputStream(new FileInputStream(COMPRESSED_FILE));
+             FileOutputStream fileOutputStream = new FileOutputStream(FILE)){
+
+            int bytes_read;
+            while ((bytes_read = gZIPInput.read(buffer)) > 0)
+                fileOutputStream.write(buffer, 0, bytes_read);
+
+            double res = (double) (System.currentTimeMillis() - start) / 1000;
+            System.out.printf("The file was decompressed successfully in %.2f sec.%n", res);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    public static void compressToGzip() throws IOException {
-        BufferedReader in = new BufferedReader(new FileReader(INPUT_FILE_TO_COMPRESS));
-        BufferedOutputStream zipOut = new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(INPUT_FILE_TO_COMPRESS + ".gzip")));
+    public static void gZip() {
+        long start = System.currentTimeMillis();
+        byte[] buffer = new byte[2048];
+        try (GZIPOutputStream gzipOuput = new GZIPOutputStream(new FileOutputStream(FILE + ".gzip"));
+             FileInputStream fileInput = new FileInputStream(FILE)) {
 
-        int c;
-        while ((c = in.read()) != -1)
-            zipOut.write(c);
+            int bytes_read;
+            while ((bytes_read = fileInput.read(buffer)) > 0) //в массив байтов buffer записываются данные
+                gzipOuput.write(buffer, 0, bytes_read);
 
-        in.close();
-        zipOut.close();
+            double res = (double) (System.currentTimeMillis() - start) / 1000;
+            System.out.printf("The file was compressed successfully in %.2f sec.%n", res);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    public static void main(String[] args) throws IOException {
-        compressToGzip();
-//                decompressGzip();
+    public static void main(String[] args) {
+        gZip();
     }
 }
